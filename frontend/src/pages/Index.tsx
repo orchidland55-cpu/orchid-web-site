b7ce6e8da818cf70809c5c3775e6319e57142b8b
orchidland55-cpu <orchidland55@gmail.com>
2026-05-18 09:42:06 +0100

    Update Index.tsx

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -19,7 +19,7 @@ const Index = () => {
         <Hero />
         <Introduction />
         <Properties />
-        <SoldProperties />
+        {/* <SoldProperties /> */}
         <News />
         <OurPartners />
         <Testimonials />

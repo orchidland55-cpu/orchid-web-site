d5cc8fc96fce7155d724b725fcf448710ebf2aaf
Perry2002 <perrydoyigbe197@gmail.com>
2026-07-02 10:23:01 +0100

    fixe deploy error

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -23,25 +23,6 @@ const Index = () => {
     <div className="min-h-screen">
       <Header />
       <main>
-<<<<<<< HEAD
-        <div className="relative">
-          <Hero />
-          {/* Desktop uniquement : la search bar n'existe pas sur mobile */}
-          <div className="hidden md:flex absolute bottom-12 left-0 right-0 translate-y-1/2 px-4 sm:px-6 z-20 justify-center animate-in fade-in slide-in-from-bottom-4 duration-700 delay-300">
-            <HeroSearchBar onSearch={(filters) => setHeroFilters(filters)} />
-          </div>
-        </div>
-
-        <div className="pt-16 md:pt-20">
-          <Properties filters={heroFilters} />
-          <PropertyCategories/>
-          <Introduction />
-          <News />
-          <OurPartners />
-          <Testimonials />
-          <FAQ />
-        </div>
-=======
         <Hero />
         {/* <div className="absolute bottom-0 left-0 right-0 translate-y-1/2 px-4 z-20">
           <div className="max-w-5xl mx-auto">
@@ -55,7 +36,6 @@ const Index = () => {
         <OurPartners />
         <Testimonials />
         <FAQ />
->>>>>>> b862036e3e4e402f40a6c4a209e28c3d5e0da6a6
       </main>
       <Footer />
       <CookieConsent />
